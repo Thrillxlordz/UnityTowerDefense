81012d5a37cba845f436844936549bdf9ee31268
Nick McCord <nickjmccord@gmail.com>
2017-03-09 10:39:11 -0500

    Main Menu Scene Update
    Updated the main menu, so that every time the scene loads, a random
    turret is chosen for display

--- TowerDefense/Assets/Scripts/MainMenu.js
+++ TowerDefense/Assets/Scripts/MainMenu.js
@@ -5,6 +5,15 @@ public var levelToLoad : String = "MainLevel";
 
 public var sceneFader : SceneFader;
 
+private var displayedTurret : GameObject;
+
+// Randomly picks a menu turret to display
+function Start() {
+    var turretChoice : int = Random.Range(0, transform.GetChild(0).transform.childCount);
+    displayedTurret = transform.GetChild(0).GetChild(turretChoice).gameObject;
+    displayedTurret.SetActive(true);
+}
+
 // Takes the player to the level select screen
 public function Play() {
 	sceneFader.FadeTo(levelToLoad);
